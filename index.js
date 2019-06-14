70ee8799c9e4360b8e930d2d884a854bd6f1a08d
Jasmine Zhang <jasmine.xuanxuan.nz@gmail.com>
2019-06-14 06:28:25 +0100

    initialize the package

---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express')
+let app = express()
+
+app.get('/', function(req,res){
+    // should return the html file here
+})
+
+app.listen(3000, function(){
+
+})
---
+++ updateDictionary.js
@@ -0,0 +1,14 @@
+class UpdateDictionary{
+    static removeDuplicate(list){
+
+    }
+
+}
+
+export default UpdateDictionary;
+
+module.exports = {
+    removeDuplicate: function(list){
+
+    }
+}
