4083658c95e6b1378b45c5aae04c18e12c2d28c4
Jasmine Zhang <jasmine.xuanxuan.nz@gmail.com>
2019-06-21 23:23:39 +0100

    add pug as the view engine, and set it up in index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
-let indexRouter = require('Routes/index');
-let vocabularyRouter = require('Routes/vocabulary');
+let indexRouter = require('./Routes/index');
+let vocabularyRouter = require('./Routes/vocabulary');
 
 app.use('/', indexRouter)
 app.use('/vocabulary',vocabularyRouter);
+app.set('views', path.join(__dirname, 'Views'));
+app.set('view engine', 'pug');
 
 app.listen(3000, function(){
 
